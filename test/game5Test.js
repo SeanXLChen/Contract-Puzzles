4865aa53337969b59d9dedefb48fd9b191b2a7b3
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-05-26 16:34:21 -0700

    Feat: solved 5th puzzle

--- test/game5Test.js
+++ test/game5Test.js
@@ -11,9 +11,26 @@ describe('Game5', function () {
   it('should be a winner', async function () {
     const { game } = await loadFixture(deployContractAndSetVariables);
 
-    // good luck
+    // Assume `suitableAddress` is an Ethereum address you control or can impersonate, 
+    // which satisfies the required condition
+    const suitableAddress = '0x00112233445566778899aabbccddeeff00112233'; // Example address
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [suitableAddress],
+    });
 
-    await game.win();
+    const signer = await ethers.getSigner(suitableAddress);
+    const defaultSigner = ethers.provider.getSigner(0); // Default signer with plenty of ETH
+
+    // Transfer ETH to the impersonated account
+    const tx = await defaultSigner.sendTransaction({
+      to: suitableAddress,
+      value: ethers.utils.parseEther("1.0") // Send 1 ETH
+    });
+    await tx.wait();
+
+    // Now attempt to win the game
+    await game.connect(signer).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
